d5adee140dc4988320cfd67f4af4f35b7c3483cb
Crusty1Bread <crisvargas3213@gmail.com>
2020-08-21 23:16:43 -0700

    created the "comm" class and made the request and present methods

---
+++ src/comm.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.lang.*;
+
+public class comm {
+    static Scanner scan = new Scanner(System.in);
+    private String userName;
+    private ArrayList<String> loc = new ArrayList<>();
+    private ArrayList<String> eventInfo = new ArrayList<>();
+
+    public comm() {
+        loc.add("gym");
+        loc.add("stem");
+        loc.add("office");
+
+        System.out.println("What is your name? ");
+        userName = scan.nextLine();
+    }
+
+    public void request() {
+        System.out.println("Please tell me what is happening? ");
+        eventInfo.add(scan.nextLine());
+
+        System.out.println("Please tell me the location of the event. ");
+        eventInfo.add(scan.nextLine());
+    }
+
+    public void present() {
+        System.out.println("Username: " + userName);
+
+        System.out.print("Saved Locations: ");
+        for (int i = 0; i < loc.size(); i++) {
+            if (i == loc.size() - 1) {
+                System.out.println(loc.get(i));
+            } else {
+                System.out.print(loc.get(i) + ", ");
+            }
+        }
+
+        System.out.println("Event Description: " + eventInfo.get(0));
+        System.out.println("Event Location: " + eventInfo.get(1));
+    }
+}
--- src/main.java
+++ src/main.java
@@ -5,6 +5,9 @@ public class main {
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        
+        comm c = new comm();
+
+        c.request();
+        c.present();
     }
 }
